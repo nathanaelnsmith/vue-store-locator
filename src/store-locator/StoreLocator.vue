edadbb0de8f1b3b632bfd33f07a0c326ab89fba4
Nathanael Smith <nathanael@ciedigital.com>
2017-07-11 16:27:19 -0700

    Setup Vuetify

---
+++ src/store-locator/StoreLocator.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-layout row wrap>
+        <v-flex xs12>
+            <h2 class="text-xs-center">Vuelp</h2>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+          <v-flex xs6>
+              <v-text-field
+                name="search-query"
+                label="Find"
+                id="search-query"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs6>
+                <v-text-field
+                  name="search-location"
+                  label="Location"
+                  id="search-location"
+                ></v-text-field>
+              </v-flex>
+      </v-layout>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+          'title': 'Store Locator'
+      }
+    }
+  }
+</script>
