84f5411ee74e06a6e55e17c0446937f24b34d758
Nathanael Smith <nathanael@ciedigital.com>
2017-07-11 16:29:34 -0700

    Fixed flex layout

--- src/store-locator/StoreLocator.vue
+++ src/store-locator/StoreLocator.vue
@@ -3,22 +3,20 @@
         <v-flex xs12>
             <h2 class="text-xs-center">Vuelp</h2>
         </v-flex>
-      </v-layout>
-      <v-layout row wrap>
-          <v-flex xs6>
-              <v-text-field
-                name="search-query"
-                label="Find"
-                id="search-query"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs6>
-                <v-text-field
-                  name="search-location"
-                  label="Location"
-                  id="search-location"
-                ></v-text-field>
-              </v-flex>
+      <v-flex sm6>
+          <v-text-field
+            name="search-query"
+            label="Find"
+            id="search-query"
+          ></v-text-field>
+        </v-flex>
+        <v-flex sm6>
+            <v-text-field
+              name="search-location"
+              label="Location"
+              id="search-location"
+            ></v-text-field>
+          </v-flex>
       </v-layout>
 </template>
 <script>
